c135b85ab978bd6c933ec8e63075eb0bb31295b1
허문혁[부동산&SME BE] <moonhyuk.hur@navercorp.com>
2024-08-06 09:17:43 +0900

    Feat: Qdrant Retriever

--- llm.py
+++ llm.py
@@ -1,11 +1,19 @@
-from insert_point_to_collection import get_point_from_collection, get_query_vector
+from qdrant_client_wrapper import QdrantClientWrapper
+from vectorstore_retriever import VectorStoreRetriever
 from rag.rag import run_rag
 
+# Qdrant
+qdrant_host = "http://3.38.106.92:6333/"
+collection_name = "company_info"
+
+# Qdrant 클라이언트 초기화
+qdrant_client_wrapper = QdrantClientWrapper(qdrant_host, collection_name)
+
 def run_llm(question: str):
-    payloads = get_point_from_collection("whole_text", get_query_vector(query_sentence=question))
-    result = run_rag(payloads, question)
+    vector_store = qdrant_client_wrapper.get_client()
+    retriever_instance = VectorStoreRetriever(vector_store)
+    result = run_rag(retriever_instance.get_retriever(), question)
     print(result)
 
-
 if __name__ == "__main__":
-    run_llm("")
+    run_llm("백엔드 개발자 5년차")
