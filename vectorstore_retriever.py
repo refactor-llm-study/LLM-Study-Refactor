79c747b9e5ff8345994654f4be5eba80a2612f82
허문혁[부동산&SME BE] <moonhyuk.hur@navercorp.com>
2024-08-31 12:10:26 +0900

    Fix: 이전 코드 삭제

--- vectorstore_retriever.py
+++
@@ -1,7 +0,0 @@
-class VectorStoreRetriever:
-    def __init__(self, vector_store, **kwargs):
-        self.vector_store = vector_store
-        self.kwargs = kwargs
-
-    def get_retriever(self, k):
-        return self.vector_store.as_retriever(k=k)
